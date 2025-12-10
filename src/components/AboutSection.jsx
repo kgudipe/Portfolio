be7421fd4aac93b876ecfb8321378a5bcfba2729
kgudipe <kgudipe.ncsu.edu>
2025-12-10 15:16:33 -0800

    Added Resume to download cv button

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -21,7 +21,7 @@ export const AboutSection = () => {
 
             <div className="flex flex-col sm:flex-row gap-4 pt-4 justify-center">
               <a href="#contact" className="cosmic-button">Get in Touch</a>
-              <a href="" className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300">Download CV</a>
+              <a href="/Koushik_Gudipelly_Resume.pdf" download className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300">Download CV</a>
             </div>
           </div>
 
